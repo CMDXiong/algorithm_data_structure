90f5d9e9f211300948be6d180e96bf092f16f799
CMDXiong <1357211280@qq.com>
2023-06-22 23:32:49 +0800

    图的遍历方式：bfs，dfs

--- Graph/bfs.py
+++ Graph/bfs.py
@@ -1,3 +1,50 @@
 # -*- coding:utf-8 -*-
 __author__ = 'px'
 
+"""
+bfs: 
+1. bfs遍历邻接矩阵
+2. bfs遍历邻接链表
+
+无权图的最短路径可以使用bfs
+"""
+
+from collections import deque
+
+
+def bfs(graph, v: int):
+    """
+    :param graph: 邻接矩阵表示的图
+    :param v: 结点标号
+    :return:
+    """
+    n = len(graph)  # 图中结点个数
+    visited = [False] * n
+    visited[v] = True
+
+    q = deque()
+    q.append(v)
+
+    while q:
+        node_num = q.popleft()
+        print(node_num)  # 打印访问的结点
+        for j in range(n):
+            if not visited[j] and graph[node_num][j] == 1:
+                visited[j] = True
+                q.append(j)
+
+
+if __name__ == "__main__":
+    graph = [
+        [0, 1, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 1, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 1, 1],
+        [0, 1, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 1],
+        [0, 0, 1, 0, 0, 0, 1, 0],
+
+    ]
+
+    bfs(graph, 0)
--- Graph/dfs.py
+++ Graph/dfs.py
@@ -1,3 +1,40 @@
 # -*- coding:utf-8 -*-
 __author__ = 'px'
 
+# 每一个点有有三种状态：1：表示未访问，2：访问但是未处理，3：已经处理
+
+
+def dfs(graph):
+    def dfs_visit(i):
+        for j in range(n):
+            if graph[i][j] == 1 and status[j] == 1:
+                status[j] = 2
+                dfs_visit(j)
+
+        status[i] = 3
+        print(i)
+
+    n = len(graph)
+    status = [1] * n
+
+    for i in range(n):
+        if status[i] == 1:
+            status[i] = 2
+            dfs_visit(i)
+
+
+if __name__ == "__main__":
+    graph = [
+        [0, 1, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 1, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 1, 1],
+        [0, 1, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 1],
+        [0, 0, 1, 0, 0, 0, 1, 0],
+
+    ]
+
+    dfs(graph)
+
