d98661a3d97f50615ec8c9d68c99f34144efbba7
CMDXiong <1357211280@qq.com>
2023-06-23 14:32:05 +0800

    图的遍历：dfs

--- Graph/dfs.py
+++ Graph/dfs.py
@@ -1,25 +1,55 @@
 # -*- coding:utf-8 -*-
 __author__ = 'px'
+"""
+时间复杂度：O(V+E)
+空间复杂度：O(V)
+"""
+
+
+from enum import Enum
+
 
 # 每一个点有有三种状态：1：表示未访问，2：访问但是未处理，3：已经处理
+class Color(Enum):
+    WHITE = 1   # 代表未访问
+    GRAY = 2    # 代表第一次被发现
+    BLACK = 3   # 代表已处理完成
+
+
+class Node:
+    def __init__(self, key):
+        self.d = 0  # 第一次被发现的时间
+        self.f = 0  # 结点搜索完成的时间
+        self.p = None  # 结点的前驱结点
+        self.color = Color.WHITE   # 结点的颜色，代表一种状态
+        self.key = key  # 结点的标号
 
 
 def dfs(graph):
     def dfs_visit(i):
+        nonlocal time
+        time += 1
+        i_node = nodes[i]
+        i_node.d = time
         for j in range(n):
-            if graph[i][j] == 1 and status[j] == 1:
-                status[j] = 2
+            j_node = nodes[j]
+            if graph[i][j] == 1 and j_node.color == Color.WHITE:
+                j_node.color = Color.GRAY
                 dfs_visit(j)
 
-        status[i] = 3
+        i_node.color = Color.BLACK
+        time += 1
+        i_node.f = time
         print(i)
 
     n = len(graph)
-    status = [1] * n
+    nodes = [Node(i) for i in range(n)]
+    time = 0
 
     for i in range(n):
-        if status[i] == 1:
-            status[i] = 2
+        node = nodes[i]
+        if node.color == Color.WHITE:
+            node.color = Color.GRAY
             dfs_visit(i)
 
 
